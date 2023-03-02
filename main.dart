aa9f28736a54fcafbf5147490abaea27734d2c99
jamie-jungah <wjddk718@gmail.com>
2023-03-03 01:32:44 +0900

    Var keyword and Dynamic

--- main.dart
+++ main.dart
@@ -1,3 +1,15 @@
+// dart try to find the main function and execute it
 void main() {
-  print('Hello World');
+  // dart need to semicolon at the end of the line
+  // vscode doesn't put semicolon at the end of the line. so we need to put it manually.
+
+  // 1. Var keyword
+  // Typically, we use var to declare a variable inside a function or a method.
+  // Also, we use type to declare a variable inside a class or declare a property.
+  var englishName = 'Jung Ah';
+  String name = '정아';
+
+  // 2. Dynamic Type
+  // Dynamic type could be anything I want.
+  dynamic age;
 }
